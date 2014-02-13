4847952182a632b6f67a39936be1ff6dcaec77ae
Brian Mann <brianmann@Brian-Manns-MacBook-Pro.local>
2014-02-12 22:22:05 -0700

    faster problem 31

--- problem31.py
+++ problem31.py
@@ -1,17 +1,18 @@
 coins = [1,2,5,10,20,50,100,200]
 
-
+cache = {}
 
 def howManyWays(amount,coinList):
     ways = 0
-
     if (amount == 0):
         return 1
     
     for x in filter(lambda y: y <= amount, coinList):
-        ways = ways + howManyWays(amount - x, filter(lambda y: y <= x, coinList))
-
+        if (amount - x, x) in cache.keys():
+            ways = ways + cache[(amount-x,x)]
+        else:
+            cache[(amount-x,x)] = howManyWays(amount - x, filter(lambda y: y <= x, coinList))
+            ways = ways + cache[(amount-x,x)]
+            
     return ways
 
-
-print howManyWays(200, coins)
